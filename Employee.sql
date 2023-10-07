27741e4f41f00e5726ac1de07c3ba7d9c6f876ed
ihtisham <ihtisham.biit.369@gmail.com>
2023-10-06 23:05:20 -0700

    Adding Queries used in this project sql server

---
+++ Employee.sql
@@ -0,0 +1,12 @@
+create table Employee
+(
+	Eid INT PRIMARY KEY,
+	EName VARCHAR(30),
+	EAGE int ,
+	ECity varchar(40),
+	ESalary int,
+	EDepart varchar(15)
+)
+insert into Employee values(01,'ihtisham',22,'Islamabad',30000,'IT')
+select * from Employee
+
---
+++ crud operatin queries used in it.sql
@@ -0,0 +1,6 @@
+use ihtisahm
+select * from Employee
+select count(Eid) from Employee where Eid=01
+update Employee set Eid=8,EName='shammm',EAGE=12,ECity='jehlum',ESalary=40000,EDepart='IT' where Eid=7
+delete Employee where Eid=8
+# remember all these queries for crud operation
